d8feae0029b974807f18390635e5ccc5bf58ae1d
Carlos-860 <carlostimotheus5@gmail.com>
2024-01-18 12:46:04 +0200

    Pages folder Commit

---
+++ pages/_document.tsx
@@ -0,0 +1,151 @@
+
+import {Head, Html, Main, NextScript} from 'next/document'
+import Script from 'next/script';
+import React from "react";
+
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			
+<Head>
+    <meta charSet="utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    {/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+    <title>Portfolio</title>
+       <meta name="description" content="Carlos Timotheus - A dedicated Full Stack Developer proficient in React js and Laravel Framework." />
+
+    {/*These are need for PWA*/}
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-title" content="Portfolio" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+    <meta name="msapplication-tap-highlight" content="no" />
+
+            <meta name="theme-color" content="#000000" />
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8"/>
+            <meta name="language" content="English"/>
+            <meta name="revisit-after" content="1 days"/>
+            <link rel="canonical" href="https://carlostimotheus5.com" />
+            <meta name="license" content="MIT License" />
+            <meta httpEquiv="content-language" content="en-us" />
+
+            <link rel="preconnect" href="https://carlostimotheus5.com" />
+
+            <link rel="dns-prefetch" href="https://carlostimotheus5.com" />
+
+            <meta name="author" content="Carlos Tiomotheus" />
+
+            <link rel="alternate" hrefLang="en" href="https://carlostimotheus5.com" />
+
+            <meta name="robots" content="index, follow" />
+            <meta name="googlebot" content="index, follow" />
+            <meta name="bingbot" content="index, follow" />
+
+            <link rel="apple-touch-icon" href="/img/logo_rounded.png" />
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/img/logo_rounded.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon.ico" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon.jpg" />
+             <link rel="icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
+            <link rel="shortcut icon" href="/favicon/favicon-org.ico" type="image/x-icon"></link>
+
+             <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+            <link rel="manifest" href="/manifest.json" />
+
+
+
+
+
+            <meta property="og:title" content="Portfolio" key="title" />
+            <meta property="og:description" content="Carlos Timotheus - A dedicated Full Stack Developer proficient in Laravel and React." />
+            <meta property="og:image" content="https://avatars.githubusercontent.com/u/74458894?v=4" />
+            <meta property="og:image:secure_url" content="https://avatars.githubusercontent.com/u/74458894?v=4" />
+            <meta property="og:image:type" content="image/jpeg" />
+            <meta property="og:image:alt" content="Portfolio" />
+            <meta property="og:image:width" content="300" />
+            <meta property="og:image:height" content="300" />
+            <meta property="og:url" content="https://carlostimotheus5.com" />
+            <meta property="og:type" content="website" />
+            <meta property="og:profile" content="https://github.com/Carlos-860" />
+            <meta property="og:site_name" content="Portfolio" />
+             <meta property="og:locale" content="en_US" />
+
+
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='2048x2732' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1668x2224' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1536x2048' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1125x2436' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='1242x2208' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='750x1334' />
+    <link rel='apple-touch-startup-image' href='/img/logo_rounded.png' sizes='640x1136' />
+
+            {/*These are the Keywords that will Boost your SEO in Ranking, so Make Sure to include and Update it up to your preference or don't mind this! 😴*/}
+            <meta name="keywords" content="
+    Full Stack Engineer, Full Stack Developer, Software Engineer,
+    Programmer, Software Developer, Portfolio Website, Web Developer, Coding, Full Stack Development,
+    Technology Enthusiast, Software Development, Computer Science, Programming Projects,
+    Frontend Developer, Backend Developer, Software Development Portfolio, Student,
+    Full Stack Web Developer, Software Engineer Portfolio,
+    Technology Projects, Software Development Engineer, Computer Programmer,
+    Code Portfolio, Software Architect, 
+    DevOps Specialist, Software Engineering Projects, Coding Portfolio,
+    Full Stack Engineer Portfolio,
+    Open Source Contributor,open source,open source projects,open source portfolio,open source contributions,
+    open source contributions portfolio,open source contributions projects,open source contributions coding,
+    open source contributions coding projects,open source contributions coding portfolio,open source contributions coding specialist,
+    open source contributions coding professional,open source contributions coding engineer,open source contributions coding developer,
+    open source contributions coding projects portfolio,open source contributions coding projects specialist,
+
+" />
+
+
+            <script
+                type="application/ld+json"
+                dangerouslySetInnerHTML={{
+                    __html: `{
+          "@context": "https://schema.org",
+          "@type": "Person",
+          "name": "Portfolio",
+          "url": "https://carlostimotheus5.com",
+          "sameAs": [
+            "https://github.com/Carlos-860",
+            "https://www.linkedin.com/in/carlos-timotheus-b276b4176/",
+          ]
+        }`
+                }}
+            />
+
+
+            {/*Edit this to your according FAQ */}
+          
+
+            {/*Edit this to your according to your website*/}
+            
+
+            {/*Edit this to your according to your website*/}
+            
+	{/*theme*/}
+	<Script id="theme.util.jsx" strategy="beforeInteractive" >
+		{`
+				let themeLocalStorage = localStorage.getItem('theme')
+				let themeSystem       = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+				document.querySelector(':root').dataset.theme = themeLocalStorage ?? themeSystem
+				`}
+	</Script>
+
+    {/* Google Analytics */}
+   
+</Head>
+
+			<body>
+			<Main />
+			<NextScript />
+			</body>
+		</Html>
+	)
+}
---
+++ pages/title.index.tsx
@@ -0,0 +1,18 @@
+
+import Head from 'next/head';
+import React from 'react';
+
+export default function TitleIndex() {
+    return (
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+            <title>Portfolio</title>
+
+            <meta name="application-name" content="Portfolio" />
+            <meta name="description" content="Carlos Timotheus - A dedicated Full Stack Developer proficient in React js and Laravel Framework." />
+            <meta name="title" content="Carlos TImotheus"/>
+        </Head>
+    );
+}
